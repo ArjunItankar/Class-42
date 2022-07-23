92e4154c13a78de158afd286361f6b9ca4e9ef1c
ArjunItankar <86589224+ArjunItankar@users.noreply.github.com>
2022-07-23 11:46:23 +1000

    Add files via upload

---
+++ js/Game.js
@@ -0,0 +1,129 @@
+class Game {
+  constructor() {
+    this.resetTitle = createElement("h2");
+    this.resetButton = createButton("");
+
+    this.leadeboardTitle = createElement("h2");
+
+    this.leader1 = createElement("h2");
+    this.leader2 = createElement("h2");
+    this.playerMoving = false;
+    this.leftKeyActive = false;
+  }
+
+  getState() {
+    var gameStateRef = database.ref("gameState");
+    gameStateRef.on("value", function(data) {
+      gameState = data.val();
+    });
+  }
+  update(state) {
+    database.ref("/").update({
+      gameState: state
+    });
+  }
+
+  start() {
+    player = new Player();
+    playerCount = player.getCount();
+
+    form = new Form();
+    form.display();
+
+    car1 = createSprite(width / 2 - 50, height - 100);
+    car1.addImage("car1", car1_img);
+    car1.scale = 0.07;
+
+    car2 = createSprite(width / 2 + 100, height - 100);
+    car2.addImage("car2", car2_img);
+    car2.scale = 0.07;
+
+    cars = [car1, car2];
+
+    fuels = new Group();
+    powerCoins = new Group();
+
+    obstacles = new Group();
+
+    var obstaclesPositions = [
+      { x: width / 2 + 250, y: height - 800, image: obstacle2Image },
+      { x: width / 2 - 150, y: height - 1300, image: obstacle1Image },
+      { x: width / 2 + 250, y: height - 1800, image: obstacle1Image },
+      { x: width / 2 - 180, y: height - 2300, image: obstacle2Image },
+      { x: width / 2, y: height - 2800, image: obstacle2Image },
+      { x: width / 2 - 180, y: height - 3300, image: obstacle1Image },
+      { x: width / 2 + 180, y: height - 3300, image: obstacle2Image },
+      { x: width / 2 + 250, y: height - 3800, image: obstacle2Image },
+      { x: width / 2 - 150, y: height - 4300, image: obstacle1Image },
+      { x: width / 2 + 250, y: height - 4800, image: obstacle2Image },
+      { x: width / 2, y: height - 5300, image: obstacle1Image },
+      { x: width / 2 - 180, y: height - 5500, image: obstacle2Image }
+    ];
+
+    // Adding fuel sprite in the game
+    this.addSprites(fuels, 4, fuelImage, 0.02);
+
+    // Adding coin sprite in the game
+    this.addSprites(powerCoins, 18, powerCoinImage, 0.09);
+
+    //Adding obstacles sprite in the game
+    this.addSprites(
+      obstacles,
+      obstaclesPositions.length,
+      obstacle1Image,
+      0.04,
+      obstaclesPositions
+    );
+  }
+
+
+  addSprites(spriteGroup, numberOfSprites, spriteImage, scale, positions = []) {
+    for (var i = 0; i < numberOfSprites; i++) {
+      var x, y;
+
+
+      //C41 //SA
+      if (positions.length > 0) {
+        x = positions[i].x;
+        y = positions[i].y;
+        spriteImage = positions[i].image;
+
+
+      } else {
+        x = random(width / 2 + 150, width / 2 - 150);
+        y = random(-height * 4.5, height - 400);
+      }
+    
+    var sprite = createSprite(x, y);
+    sprite.addImage("sprite", spriteImage);
+
+    sprite.scale = scale;
+    spriteGroup.add(sprite);
+    }
+   }
+
+
+   handleElements() {
+     form.hide();
+     form.titleImg.position(40, 50);
+     form.titleImg.class("gameTitleAfterEffect");
+
+     
+     this.resetTitle.html("Reset Game");
+     this.resetTitle.class("resetText");
+     this.resetTitle.position(width / 2 + 200, 40);
+
+     this.resetButton.class("resetButton");
+     this.resetButton.position(width / 2 + 230, 100);
+
+     this.leadeboardTitle.html("Leaderboard");
+     this.leaderboardTitle.class("leaderboardText");
+     this.leaderboardTitle.position(width / 3 - 60, 40);
+
+     this.leader1.class("leadersText");
+     this.leader1.position(width / 3 - 50, 80);
+
+     this.leader2.class("leadersText");
+     this.leader2.position(width / 3 - 50, 130);
+   }
+}
